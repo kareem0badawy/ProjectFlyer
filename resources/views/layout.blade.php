e09b55bd658baf4db2e1fff630f75593a8903fca
Kareem <kareem0badawy>
2016-04-13 14:48:43 +0200

    edits

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Project Flyer</title>
+	<link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+		<div class="container">
+			@yield('content')
+		</div>
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('content')
+
+
+	<h1 style="color: #000">Project Flyer</h1>
+
+@stop
